21c375f8e89f5286c33933b2e815d4ceaee114b2
Carlo Ettisberger <carlo_ettisberger@hotmail.com>
2024-11-01 12:30:01 +0100

    works finally

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import { Metadata } from "next";
 
-import { SliceZone } from "@prismicio/react";
+import { PrismicRichText, SliceZone } from "@prismicio/react";
 import * as prismic from "@prismicio/client";
 
 import { createClient } from "@/prismicio";
@@ -14,7 +14,7 @@ import { components } from "@/slices";
 
 export async function generateMetadata(): Promise<Metadata> {
   const client = createClient();
-  const home = await client.getByUID("page", "home");
+  const home = await client.getByUID("page", "home")
 
   return {
     title: prismic.asText(home.data.title),
@@ -31,5 +31,5 @@ export default async function Index() {
   const client = createClient();
   const home = await client.getByUID("page", "home");
 
-  return <SliceZone slices={home.data.slices} components={components} />;
+  return <PrismicRichText field={home.data.title} />;
 }
