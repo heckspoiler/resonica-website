f52e5111df3a72db2e88eec9348748cc127e2e9a
Carlo Ettisberger <carlo_ettisberger@hotmail.com>
2024-11-09 13:12:15 +0100

    chagnes

--- src/app/components/BackgroundComponents/BackgroundCanvas.tsx
+++ src/app/components/BackgroundComponents/BackgroundCanvas.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { useRef, useEffect } from 'react';
+import React, { useRef, useEffect, useState } from 'react';
 import {
   Scene,
   OrthographicCamera,
@@ -19,12 +19,14 @@ import vertex from './shaders/background.vert';
 
 // Import the image as a texture
 import backgroundDesktop from '../../../../public/blurry2.png';
+import backgroundMobile from '../../../../public/background_mobile.png';
 
 export default function BackgroundCanvas() {
   const mountRef = useRef<HTMLDivElement | null>(null);
+  const [background, setBackground] = useState<string>(backgroundMobile.src);
 
   // Mouse and position variables
-  const easeFactor = useRef<number>(0.8);
+  const easeFactor = useRef<number>(0.1);
   const mousePosition = useRef<{ x: number; y: number }>({ x: 0.5, y: 0.5 });
   const targetMousePosition = useRef<{ x: number; y: number }>({
     x: 0.5,
@@ -33,34 +35,52 @@ export default function BackgroundCanvas() {
   const prevPosition = useRef<{ x: number; y: number }>({ x: 0.5, y: 0.5 });
 
   useEffect(() => {
+    // Update background based on screen size
+    const updateBackground = () => {
+      if (window.innerWidth < 768) {
+        setBackground(backgroundMobile.src);
+      } else {
+        setBackground(backgroundDesktop.src);
+      }
+    };
+
+    updateBackground();
+    window.addEventListener('resize', updateBackground);
+
+    return () => {
+      window.removeEventListener('resize', updateBackground);
+    };
+  }, []);
+
+  useEffect(() => {
     // Scene setup
     const scene = new Scene();
 
-    // Camera setup
+    // Orthographic camera setup
     const aspectRatio = window.innerWidth / window.innerHeight;
     const camera = new OrthographicCamera(
-      -1,
+      -aspectRatio,
+      aspectRatio,
       1,
-      (1 / aspectRatio) * 1.8,
-      (-1 / aspectRatio) * 1.8,
+      -1,
       0.1,
       1000
     );
-    camera.position.z = 15;
+    camera.position.z = 1; // Position the camera close to the plane
 
     // Texture and Shader Material setup
     const loader = new TextureLoader();
-    const texture: Texture = loader.load(backgroundDesktop.src);
+    const texture: Texture = loader.load(background);
     const shaderUniforms = {
       u_texture: { value: texture },
       u_mouse: { value: new Vector2(0.5, 0.5) },
       u_prevMouse: { value: new Vector2(0.5, 0.5) },
       u_hoverColor: { value: new Vector3(1.0, 0.0, 0.0) },
       u_gravityStrength: { value: 1.8 },
-      u_time: { value: 0.0 * 0.005 }, // Initialize u_time to 0.0
+      u_time: { value: 0.0 },
     };
 
-    const planeGeometry = new PlaneGeometry(2, 2);
+    const planeGeometry = new PlaneGeometry(2 * aspectRatio, 2);
     const planeMaterial = new ShaderMaterial({
       vertexShader: vertex,
       fragmentShader: fragment,
@@ -75,7 +95,7 @@ export default function BackgroundCanvas() {
     const renderer = new WebGLRenderer({ antialias: true });
     renderer.setClearColor(0xffffff, 1);
     renderer.setSize(window.innerWidth, window.innerHeight);
-    renderer.setPixelRatio(window.devicePixelRatio);
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2)); // Cap device pixel ratio to improve performance
 
     if (mountRef.current) {
       mountRef.current.appendChild(renderer.domElement);
@@ -83,7 +103,6 @@ export default function BackgroundCanvas() {
 
     // Mouse event listeners
     const handleMouseMove = (event: MouseEvent) => {
-      easeFactor.current = 0.08;
       const rect = mountRef.current?.getBoundingClientRect();
       if (!rect) return;
 
@@ -92,33 +111,13 @@ export default function BackgroundCanvas() {
       targetMousePosition.current.y = (event.clientY - rect.top) / rect.height;
     };
 
-    const handleMouseEnter = (event: MouseEvent) => {
-      easeFactor.current = 0.01;
-      const rect = mountRef.current?.getBoundingClientRect();
-      if (!rect) return;
-
-      mousePosition.current.x = targetMousePosition.current.x =
-        (event.clientX - rect.left) / rect.width;
-      mousePosition.current.y = targetMousePosition.current.y =
-        (event.clientY - rect.top) / rect.height;
-    };
-
-    const handleMouseLeave = () => {
-      easeFactor.current = 0.01;
-      targetMousePosition.current = { ...prevPosition.current };
-    };
-
     mountRef.current?.addEventListener('mousemove', handleMouseMove);
-    mountRef.current?.addEventListener('mouseenter', handleMouseEnter);
-    mountRef.current?.addEventListener('mouseleave', handleMouseLeave);
 
     // Animation loop
     const animateScene = () => {
       requestAnimationFrame(animateScene);
 
-      // Update u_time uniform
-      shaderUniforms.u_time.value += 0.01; // Increment time, adjust speed as needed
-
+      // Smooth animation updates
       mousePosition.current.x +=
         (targetMousePosition.current.x - mousePosition.current.x) *
         easeFactor.current;
@@ -126,6 +125,8 @@ export default function BackgroundCanvas() {
         (targetMousePosition.current.y - mousePosition.current.y) *
         easeFactor.current;
 
+      // Update uniforms
+      shaderUniforms.u_time.value += 0.01; // Control time increments for smoother animation
       planeMaterial.uniforms.u_mouse.value.set(
         mousePosition.current.x,
         1.0 - mousePosition.current.y
@@ -138,15 +139,13 @@ export default function BackgroundCanvas() {
       renderer.render(scene, camera);
     };
 
-    animateScene();
-
-    // Resize event listener
+    // Resize handling for Three.js
     const onWindowResize = () => {
-      const aspectRatio = window.innerWidth / window.innerHeight;
-      camera.left = -1;
-      camera.right = 1;
-      camera.top = (1 / aspectRatio) * 1.8;
-      camera.bottom = (-1 / aspectRatio) * 1.8;
+      const newAspectRatio = window.innerWidth / window.innerHeight;
+      camera.left = -newAspectRatio;
+      camera.right = newAspectRatio;
+      camera.top = 1;
+      camera.bottom = -1;
       camera.updateProjectionMatrix();
 
       renderer.setSize(window.innerWidth, window.innerHeight);
@@ -154,18 +153,18 @@ export default function BackgroundCanvas() {
 
     window.addEventListener('resize', onWindowResize);
 
+    animateScene();
+
     return () => {
       window.removeEventListener('resize', onWindowResize);
       mountRef.current?.removeEventListener('mousemove', handleMouseMove);
-      mountRef.current?.removeEventListener('mouseenter', handleMouseEnter);
-      mountRef.current?.removeEventListener('mouseleave', handleMouseLeave);
 
       renderer.dispose();
       if (mountRef.current) {
         mountRef.current.removeChild(renderer.domElement);
       }
     };
-  }, []);
+  }, [background]); // Re-run effect when background changes
 
   return <div ref={mountRef} style={{ width: '100vw', height: '100vh' }} />;
 }
