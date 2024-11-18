9713a7dd1f845736aa84d651afa49037db15ef49
Carlo Ettisberger <carlo_ettisberger@hotmail.com>
2024-11-18 10:18:30 +0100

    changes

--- src/app/components/BackgroundComponents/BackgroundCanvas.tsx
+++ src/app/components/BackgroundComponents/BackgroundCanvas.tsx
@@ -37,7 +37,7 @@ export default function BackgroundCanvas() {
   useEffect(() => {
     // Update background based on screen size
     const updateBackground = () => {
-      if (window.innerWidth < 850) {
+      if (window.innerWidth < 1120) {
         setBackground(backgroundMobile.src);
       } else {
         setBackground(backgroundDesktop.src);
